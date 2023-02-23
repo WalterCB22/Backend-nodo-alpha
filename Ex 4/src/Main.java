1310827221db9456275667145848dac80393c4f0
Bryan <avendanoparrab@gmail.com>
2023-02-23 17:19:00 -0500

    Update Commit

--- Ex 4/src/Main.java
+++ Ex 4/src/Main.java
@@ -5,20 +5,28 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner src = new Scanner(System.in);
-
-        System.out.print("Ingresa un valor: ");
-        int valor = src.nextInt();
-
-        int variable = 0;
-        while (valor !=variable){
-            
-
-
-            variable++;
+        Scanner scr = new Scanner(System.in);
+        int num = scr.nextInt();
+        System.out.println("Ingrese un numero");
+        for (int x = 2; x <= num; x++) {
+            if (esPrimo(x)) {
+                System.out.println(x + " es primo");
+            }
         }
 
-
     }
 
+
+    public static boolean esPrimo(int numero) {
+        int contador = 2;
+        boolean primo = true;
+        while ((primo) && (contador != numero)) {
+            if (numero % contador == 0)
+                primo = false;
+            contador++;
+        }
+        return primo;
     }
+}
+
+
